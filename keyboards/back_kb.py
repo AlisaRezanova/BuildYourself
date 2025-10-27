d6b2246de78c0b792db9f3aea4f89df2039c88b2
4319 Резанова А.Р. <alsa2002g@gmail.com>
2025-10-27 22:37:52 +0300

    .

--- keyboards/back_kb.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
-
-def back_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text="Главное меню")
-    kb.adjust(2)
-    return kb.as_markup(resize_keyboard=True)
